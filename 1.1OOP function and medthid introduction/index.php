57f869237dc8254786331576eb84710285f60c38
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2024-10-01 04:47:28 +0600

    Add files via upload

---
+++ 1.1OOP function and medthid introduction/index.php
@@ -0,0 +1,16 @@
+<?php
+class lastName{
+	public $name='Ibrahim';
+	public $age='30';
+	public $address='Dhaka';
+
+	// function info()
+	// {
+	// 	$this->name = "Village";
+	// 	return $this->name;
+	// }
+}
+//$ibrahim = new main();
+$l = new lastName();
+$l->name = "Khan"; // Assign the value
+echo $l->name; // Output the value
