6dcdf28443d9cc2b12a188ba22cbe7e647161567
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2024-10-01 19:04:04 +0600

    Add files via upload

---
+++ 2.5. final Keyword- OOP - inheritance/index.php
@@ -0,0 +1,14 @@
+<?php
+final channel
+{
+    public function tv(){
+        return "thanks for watching my channel";
+    }
+}
+class phpvideo extends channel{
+    public function tv(){
+        return "this oop video";
+    }
+}
+
+?>
